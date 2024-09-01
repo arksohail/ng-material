66df560a5ff10973f84c05cfabddbc6cb9b3ed93
Shaikh Abdul Sohail <sohail@papa.fit>
2024-09-01 23:44:13 +0530

    Added Route to Buttons Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
+import { ButtonComponent } from './pages/button/button.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'main', pathMatch: 'full' },
-  { path: 'main', component: MainComponent }
+  { path: 'main', component: MainComponent },
+  { path: 'buttons', component: ButtonComponent }
 ];
 
 @NgModule({
