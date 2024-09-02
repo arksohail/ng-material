37df7d0a5e750289204e56cf72e35ccd6e06609a
Shaikh Abdul Sohail <sohail@papa.fit>
2024-09-02 20:19:30 +0530

    Added Routing to Menu Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { ButtonToggleComponent } from './pages/button-toggle/button-toggle.compo
 import { DynamicColorsComponent } from './pages/dynamic-colors/dynamic-colors.component';
 import { ElevationComponent } from './pages/elevation/elevation.component';
 import { RippleComponent } from './pages/ripple/ripple.component';
+import { MenuComponent } from './pages/menu/menu.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'main', pathMatch: 'full' },
@@ -15,6 +16,7 @@ const routes: Routes = [
   { path: 'dynamic-color', component: DynamicColorsComponent },
   { path: 'elevation', component: ElevationComponent },
   { path: 'ripple', component: RippleComponent },
+  { path: 'menu', component: MenuComponent },
 ];
 
 @NgModule({
